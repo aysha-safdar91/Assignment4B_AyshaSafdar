2703f41be7580636a661549f6b933ccc315004f9
aysha-safdar91 <61646988+aysha-safdar91@users.noreply.github.com>
2020-07-21 03:21:19 +0500

    Project

---
+++ Hummingbirds/Assets/Scripts/wait.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class wait : MonoBehaviour
+{
+   public void pengiun()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void humming_bird()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void backtomain()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
